1ceefc71216efa73faf10d1cbab65e908f19f25c
CamilaGO <gon18398@uvg.edu.gt>
2020-09-09 21:48:08 -0500

    ya funciona

---
+++ lib.py
@@ -0,0 +1,192 @@
+from collections import namedtuple
+import struct
+import numpy
+
+# ===============================================================
+# Paula Camila Gonzalez Ortega - 18398
+# ===============================================================
+
+class V3(object):
+  def __init__(self, x, y = None, z = None):
+    if (type(x) == numpy.matrix):
+      self.x, self.y, self.z = x.tolist()[0]
+    else:
+      self.x = x
+      self.y = y
+      self.z = z
+
+  def __repr__(self):
+    return "V3(%s, %s, %s)" % (self.x, self.y, self.z)
+
+class V2(object):
+  def __init__(self, x, y = None):
+    if (type(x) == numpy.matrix):
+      self.x, self.y = x.tolist()[0]
+    else:
+      self.x = x
+      self.y = y
+
+  def __repr__(self):
+    return "V2(%s, %s)" % (self.x, self.y)
+
+
+def sum(v0, v1):
+  # suma dos vectores de 3 elementos 
+  return V3(v0.x + v1.x, v0.y + v1.y, v0.z + v1.z)
+
+def sub(v0, v1):
+  # resta dos vectores de 3 elementos
+  return V3(v0.x - v1.x, v0.y - v1.y, v0.z - v1.z)
+
+def mul(v0, k):
+  # multiplica un vector de 3 elementos por una constante
+  return V3(v0.x * k, v0.y * k, v0.z *k)
+
+def dot(v0, v1):
+  # reliza el producto punto de dos vectores de 3 elementos 
+  # el resultado es un escalar
+  return v0.x * v1.x + v0.y * v1.y + v0.z * v1.z
+
+def cross(v1, v2):
+  return V3(
+    v1.y * v2.z - v1.z * v2.y,
+    v1.z * v2.x - v1.x * v2.z,
+    v1.x * v2.y - v1.y * v2.x,
+  )
+
+def length(v0):
+  # devuelve el tamaño (escalar) del vector
+  return (v0.x**2 + v0.y**2 + v0.z**2)**0.5
+
+def norm(v0):
+  #calcula la normal de un vector de 3 elementos
+  v0length = length(v0)
+
+  if not v0length:
+    return V3(0, 0, 0)
+
+  return V3(v0.x/v0length, v0.y/v0length, v0.z/v0length)
+
+def bbox(*vertices):
+  # Se reciben *n vectores de 2 elementos para encontrar los x,y maximos y minimos
+  # para poder hacer la boundingbox, es decir cubrir el poligono
+  xs = [ vertex.x for vertex in vertices ]
+  ys = [ vertex.y for vertex in vertices ]
+
+  return (max(xs), max(ys), min(xs), min(ys))
+
+def barycentric(A, B, C, P):
+  # Este algoritmo de numeros baricentricos sirve para llena un poligono
+  # Parametros: 3 vectores de 2 elementos y un punto
+  # Return: 3 coordinadas baricentricas del punto segun el triangulo formado a partir de los vectores
+  bc = cross(
+    V3(B.x - A.x, C.x - A.x, A.x - P.x), 
+    V3(B.y - A.y, C.y - A.y, A.y - P.y)
+  )
+
+  if abs(bc.z) < 1:
+    return -1, -1, -1   # no es un triangulo de verdad, no devuelve nada afuera
+
+  # [cx cy cz] == [u v 1]
+
+  u = bc.x/bc.z
+  v = bc.y/bc.z
+  w = 1 - (bc.x + bc.y)/bc.z
+
+  return w, v, u
+
+
+def MultMatriz(a,b):
+  #es funcion permite multiplicar matrices sin uso de libreria
+  c = []
+  for i in range(0,len(a)):
+    temp=[]
+    for j in range(0,len(b[0])):
+      s = 0
+      for k in range(0,len(a[0])):
+        #multiplicacion de item por item entre la fila y columna de las matrices
+        s += a[i][k]*b[k][j]
+      temp.append(s)
+    c.append(temp)
+  return c
+
+def reflect(I, N):
+  Lm = mul(I, -1)
+  n = mul(N, 2 * dot(Lm, N))
+  return norm(sub(Lm, n))
+
+def char(c):
+  return struct.pack('=c', c.encode('ascii'))
+
+def word(w):
+  return struct.pack('=h', w)
+
+def dword(d):
+  return struct.pack('=l', d)
+
+# ===============================================================
+# BMP
+# ===============================================================
+
+def writebmp(filename, width, height, pixels):
+  f = open(filename, 'bw')
+
+  # File header (14 bytes)
+  f.write(char('B'))
+  f.write(char('M'))
+  f.write(dword(14 + 40 + width * height * 3))
+  f.write(dword(0))
+  f.write(dword(14 + 40))
+
+  # Image header (40 bytes)
+  f.write(dword(40))
+  f.write(dword(width))
+  f.write(dword(height))
+  f.write(word(1))
+  f.write(word(24))
+  f.write(dword(0))
+  f.write(dword(width * height * 3))
+  f.write(dword(0))
+  f.write(dword(0))
+  f.write(dword(0))
+  f.write(dword(0))
+
+  # Pixel data (width x height x 3 pixels)
+  for x in range(height):
+    for y in range(width):
+      f.write(pixels[x][y].toBytes())
+  f.close()
+
+# ===============================================================
+# COLOR CLASS
+# ===============================================================
+
+class color(object):
+  def __init__(self, r, g, b):
+    self.r = r
+    self.g = g
+    self.b = b
+
+  def __add__(self, other_color):
+    r = self.r + other_color.r
+    g = self.g + other_color.g
+    b = self.b + other_color.b
+
+    return color(r, g, b)
+
+  def __mul__(self, other):
+    r = self.r * other
+    g = self.g * other
+    b = self.b * other
+    return color(r, g, b)
+
+  def __repr__(self):
+    return "color(%s, %s, %s)" % (self.r, self.g, self.b)
+
+  def toBytes(self):
+    self.r = int(max(min(self.r, 255), 0))
+    self.g = int(max(min(self.g, 255), 0))
+    self.b = int(max(min(self.b, 255), 0))
+    return bytes([self.b, self.g, self.r])
+
+  __rmul__ = __mul__
---
+++ raytracer.py
@@ -0,0 +1,129 @@
+from lib import *
+from sphere import *
+from math import pi, tan
+
+BLACK = color(0, 0, 0)
+WHITE = color(255, 255, 255)
+FONDO = color(0, 0, 250)
+
+
+class Raytracer(object):
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+    self.background_color = BLACK
+    self.light = None
+    self.scene = []
+    self.clear()
+
+  def clear(self):
+    self.pixels = [
+      [self.background_color for x in range(self.width)]
+      for y in range(self.height)
+    ]
+
+  def write(self, filename):
+  	writebmp(filename, self.width, self.height, self.pixels)
+
+  def display(self, filename='out.bmp'):
+  	self.render()
+  	self.write(filename)
+
+  def point(self, x, y, c = None):
+    try:
+      self.pixels[y][x] = c or self.current_color
+    except:
+      pass
+
+  def scene_intersect(self, orig, direction):
+    zbuffer = float('inf')
+
+    material = None
+    intersect = None
+
+    for obj in self.scene:
+      hit = obj.ray_intersect(orig, direction)
+      if hit is not None:
+        if hit.distance < zbuffer:
+          zbuffer = hit.distance
+          material = obj.material
+          intersect = hit
+
+    return material, intersect
+
+  def cast_ray(self, orig, direction):
+    material, intersect = self.scene_intersect(orig, direction)
+
+    if material is None:
+      return self.background_color
+
+    light_dir = norm(sub(self.light.position, intersect.point))
+    light_distance = length(sub(self.light.position, intersect.point))
+
+    offset_normal = mul(intersect.normal, 1.1)  # avoids intercept with itself
+    shadow_orig = sub(intersect.point, offset_normal) if dot(light_dir, intersect.normal) < 0 else sum(intersect.point, offset_normal)
+    shadow_material, shadow_intersect = self.scene_intersect(shadow_orig, light_dir)
+    shadow_intensity = 0
+
+    if shadow_material and length(sub(shadow_intersect.point, shadow_orig)) < light_distance:
+      shadow_intensity = 0.9
+
+    intensity = self.light.intensity * max(0, dot(light_dir, intersect.normal)) * (1 - shadow_intensity)
+
+    reflection = reflect(light_dir, intersect.normal)
+    specular_intensity = self.light.intensity * (
+      max(0, -dot(reflection, direction))**material.spec
+    )
+
+    diffuse = material.diffuse * intensity * material.albedo[0]
+    specular = color(255, 255, 255) * specular_intensity * material.albedo[1]
+    return diffuse + specular
+
+  """def cast_ray(self, orig, direction, sphere):
+    if sphere.ray_intersect(orig, direction):
+      return color(255, 0, 0)
+    else:
+      return color(0, 0, 255)"""
+
+  def render(self):
+    alfa = int(pi/2)
+    for y in range(self.height):
+      for x in range(self.width):
+        i =  (2*(x + 0.5)/self.width - 1)*self.width/self.height*tan(alfa/2)
+        j =  (2*(y + 0.5)/self.height - 1 )*tan(alfa/2)
+        # x = int(x)
+        # y = int(y)
+        # print(x, y)
+        direction = norm(V3(i, j, -1))
+        self.pixels[y][x] = self.cast_ray(V3(0,0,0), direction)
+
+  """def basicRender(self):
+  #Esto llena la pantalla de un color degradado
+    for x in range(self.width):
+      for y in range(self.height):
+        r = int((x/self.width)*255) if x/self.width < 1 else 1
+        g = int((y/self.height)*255) if y/self.height < 1 else 1
+        b = 0
+        self.pixels[y][x] = color(r, g, b)"""
+
+ivory = Material(diffuse=color(100, 100, 80), albedo=(0.6,  0.3), spec=50)
+rubber = Material(diffuse=color(80, 0, 0), albedo=(0.9,  0.1), spec=10)
+
+r = Raytracer(1000, 1000)
+
+r.light = Light(
+  position=V3(-20, 20, 20),
+  intensity=1.5
+)
+
+r.background_color = FONDO
+
+r.scene = [
+  Sphere(V3(0, -1.5, -10), 1.5, ivory),
+  Sphere(V3(-2, -1, -12), 2, rubber),
+  Sphere(V3(1, 1, -8), 1.7, rubber),
+  Sphere(V3(0, 2, -10), 2, ivory)
+]
+#s = Sphere(V3(0, 0, -16), 2)
+#r.render()
+r.display()
